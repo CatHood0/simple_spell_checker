12e58364e4b70fe38c6909de708fee6018744e85
CatHood0 <santiagowmar@gmail.com>
2024-08-18 00:29:43 -0400

    Doc: more docs for LanguageIdentifier class

--- lib/src/common/language_identifier.dart
+++ lib/src/common/language_identifier.dart
@@ -1,3 +1,22 @@
+/// [LanguageIdentifier] is a representation of a register of a language with it's dictionary
+///   [language] ref to the country code 
+///   [word] ref to the dictionary
+///
+/// [words] dictionary must have a plain text format where the new lines separate every word
+///
+/// # Example:
+/// it language should have a dictionary format like:
+///
+/// italia
+/// miniatura
+/// famiglia
+/// specie
+/// europa
+/// latino
+/// animalia
+/// roma
+///
+/// Making simple and easier add custom dictionaries for everyone
 class LanguageIdentifier {
   final String language;
   final String words;
