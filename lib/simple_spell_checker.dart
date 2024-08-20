b350c763a48687eb37f29e491310a7ffd63d5c4a
CatHood0 <santiagowmar@gmail.com>
2024-08-20 07:40:24 -0400

    Fix: strategy enum is not exported

--- lib/simple_spell_checker.dart
+++ lib/simple_spell_checker.dart
@@ -2,6 +2,7 @@ library simple_spell_checker;
 
 export '/src/word_tokenizer.dart';
 export 'src/common/language_dic_priority_order.dart';
+export 'src/common/strategy_language_search_order.dart';
 export 'src/common/language_identifier.dart';
 export 'src/common/extensions.dart';
 export 'src/common/tokenizer.dart';
