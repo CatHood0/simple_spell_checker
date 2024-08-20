c7314ae405918e36621db8b4b97fbbd4f7df4c30
CatHood0 <santiagowmar@gmail.com>
2024-08-20 07:52:43 -0400

    Chore: Added more test for dispose error cases and italian

--- test/simple_spell_checker_test.dart
+++ test/simple_spell_checker_test.dart
@@ -2,16 +2,15 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:simple_spell_checker/src/spell_checker.dart';
 
 void main() {
-  test('Should return a simple map with right parts and wrong parts in english',
-      () {
+  test('Should return a simple map with right parts and wrong parts in english', () {
     TestWidgetsFlutterBinding.ensureInitialized();
     final SimpleSpellChecker spellchecker = SimpleSpellChecker(
       language: 'en',
       safeDictionaryLoad: false,
     );
     //spellchecker.testingMode = true;
-    final content = spellchecker.checkBuilder<Map<String, bool>>(
-        'this is a tśr (with) sme errors.', builder: (word, isWrong) {
+    final content =
+        spellchecker.checkBuilder<Map<String, bool>>('this is a tśr (with) sme errors.', builder: (word, isWrong) {
       return {word: isWrong};
     });
     expect(content, isNotNull);
@@ -30,23 +29,16 @@ void main() {
       {'errors': false}, // is not wrong
       {'.': false}, // is not wrong
     ]);
-    spellchecker.dispose();
-    // this should throws an error
-    expect(() {
-      spellchecker.registerLanguage('Shouldn\'t add this language');
-    }, throwsA(isA<AssertionError>()));
   });
 
-  test('Should return a simple map with right parts and wrong parts in deutsch',
-      () {
+  test('Should return a simple map with right parts and wrong parts in deutsch', () {
     TestWidgetsFlutterBinding.ensureInitialized();
     final SimpleSpellChecker spellchecker = SimpleSpellChecker(
       language: 'de',
       safeDictionaryLoad: false,
     );
     //spellchecker.testingMode = true;
-    final content = spellchecker.checkBuilder<Map<String, bool>>(
-        'Dies ist ein Tst (Äpfel, Männer) eiige Fehler',
+    final content = spellchecker.checkBuilder<Map<String, bool>>('Dies ist ein Tst (Äpfel, Männer) eiige Fehler',
         builder: (word, isWrong) {
       return {word: isWrong};
     });
@@ -67,23 +59,46 @@ void main() {
       {' ': false}, // is not wrong
       {'Fehler': false}, // is not wrong
     ]);
-    spellchecker.dispose();
-    // this should throws an error
-    expect(() {
-      spellchecker.registerLanguage('Shouldn\'t add this language');
-    }, throwsA(isA<AssertionError>()));
   });
 
-  test('Should return a simple map with right parts and wrong parts in spanish',
-      () {
+  test('Should return a simple map with right parts and wrong parts in italian', () {
+    TestWidgetsFlutterBinding.ensureInitialized();
+    final SimpleSpellChecker spellchecker = SimpleSpellChecker(
+      language: 'it',
+      safeDictionaryLoad: false,
+    );
+    //spellchecker.testingMode = true;
+    final content = spellchecker.checkBuilder<Map<String, bool>>('Questo è un test (con) aluni errori grammaticali',
+        builder: (word, isWrong) {
+      return {word: isWrong};
+    });
+    expect(content, isNotNull);
+    expect(content, isNotEmpty);
+    expect(content, [
+      {'Questo ': false}, // is not wrong
+      {'è ': false}, // is not wrong
+      {'un ': false}, // is not wrong
+      {'test': true}, // is wrong
+      {' ': false}, // is wrong
+      {'(': false},
+      {'con': false}, // is not wrong
+      {') ': false},
+      {'aluni': true}, // is wrong
+      {' ': false}, // is not wrong
+      {'errori ': false}, // is not wrong
+      {'grammaticali': false}, // is not wrong
+    ]);
+  });
+
+  test('Should return a simple map with right parts and wrong parts in spanish', () {
     TestWidgetsFlutterBinding.ensureInitialized();
     final SimpleSpellChecker spellchecker = SimpleSpellChecker(
       language: 'es',
       safeDictionaryLoad: false,
     );
     //spellchecker.testingMode = true;
-    final content = spellchecker.checkBuilder<Map<String, bool>>(
-        'Ésto es un tesr (con) alnu errores', builder: (word, isWrong) {
+    final content = spellchecker.checkBuilder<Map<String, bool>>('Ésto es un tesr (con) alnu errores',
+        builder: (word, isWrong) {
       return {word: isWrong};
     });
     expect(content, isNotNull);
@@ -101,10 +116,21 @@ void main() {
       {' ': false}, // is not wrong
       {'errores': false}, // is not wrong
     ]);
+  });
+
+  test('Should dispose service and throw error when try to use checkBuilder() method', () {
+    TestWidgetsFlutterBinding.ensureInitialized();
+    final SimpleSpellChecker spellchecker = SimpleSpellChecker(
+      language: 'de',
+      safeDictionaryLoad: false,
+    );
     spellchecker.dispose();
     // this should throws an error
     expect(() {
-      spellchecker.registerLanguage('Shouldn\'t add this language');
+      spellchecker.checkBuilder<Map<String, bool>>('Dies ist ein Tst (Äpfel, Männer) eiige Fehler',
+          builder: (word, isWrong) {
+        return {word: isWrong};
+      });
     }, throwsA(isA<AssertionError>()));
   });
 }
