2579df6d710da5948651f6b8cb89c6cb889cb3f1
CatHood0 <santiagowmar@gmail.com>
2024-08-21 05:31:17 -0400

    Fix: uppercase words with one char are not parsed to lowercase

--- lib/src/common/extensions.dart
+++ lib/src/common/extensions.dart
@@ -2,7 +2,7 @@ extension WordExtension on String {
   String capitalize() =>
       isEmpty || length < 2 ? this : "${this[0].toUpperCase()}${substring(1)}";
   String toLowerCaseFirst() =>
-      isEmpty || length < 2 ? this : "${this[0].toLowerCase()}${substring(1)}";
+      isEmpty || length < 2 ? toLowerCase() : "${this[0].toLowerCase()}${substring(1)}";
   bool get noWords => RegExp(
           r'''[,;:'`´^¨`\'\.°\|\*\•µ\[\]\(\)\!\¡\¿\?\¶\$\%\&\/\\=\}\{\+\-©℗ⓒ·~½¬ſþˀ\_\«\»\<\>\¢\@\€\←\↓\→\ð\ø\¢\”\“\„\"]''')
       .hasMatch(this);
