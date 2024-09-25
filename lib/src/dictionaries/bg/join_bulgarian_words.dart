e4eada28a20e6491e28b2527cde74f4a89d30649
CatHood0 <santiagowmar@gmail.com>
2024-09-25 01:27:16 -0400

    Feat: support for register bulgarian language

--- lib/src/dictionaries/bg/join_bulgarian_words.dart
+++
@@ -1,4 +0,0 @@
-import 'package:simple_spell_checker/src/dictionaries/bg/bg_words1.dart';
-import 'package:simple_spell_checker/src/dictionaries/utils/dic_utils.dart';
-
-final joinBulgarianWords = removeUnnecessaryCharacters(bgWords1);
---
+++ simple_spell_checker_bg_lan/lib/src/simple_spell_checker_bg.dart
@@ -0,0 +1,33 @@
+
+// ignore: depend_on_referenced_packages
+import 'dart:convert';
+import 'package:simple_spell_checker/simple_spell_checker.dart' show SimpleSpellChecker, languagesToBeUsed;
+import 'package:simple_spell_checker_bg_lan/src/bg/join_bulgarian_words.dart';
+
+class SimpleSpellCheckerBgRegister {
+  static const _splitter = LineSplitter();
+
+  /// `registerBulgarianLanguage` can be used to register manually the arabic 
+  /// language to be supported by the `SimpleSpellChecker`
+  static void registerBulgarianLanguage() {
+    if (languagesToBeUsed.containsKey('bg')) return;
+    SimpleSpellChecker.setLanguage('gb', _createDictionary(joinBulgarianWords));
+  }
+
+  static Map<String, int> _createDictionary(String words) {
+    if (words.trim().isEmpty) {
+      return {};
+    }
+    final Iterable<MapEntry<String, int>> entries = _splitter.convert(words).map(
+          (element) => MapEntry(
+            element.trim().toLowerCase(),
+            1,
+          ),
+        );
+    return {}..addEntries(entries);
+  }
+
+  static void unRegisterBulgarianLanguage() {
+    languagesToBeUsed.remove('bg');
+  }
+}
