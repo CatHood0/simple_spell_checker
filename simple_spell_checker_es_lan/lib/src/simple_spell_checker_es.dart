28d2b8d0b0ce388986f02393e52f68e4a1b2d354
CatHood0 <santiagowmar@gmail.com>
2024-09-25 01:09:41 -0400

    Feat: support for register spanish language

---
+++ simple_spell_checker_es_lan/lib/src/simple_spell_checker_es.dart
@@ -0,0 +1,32 @@
+// ignore: depend_on_referenced_packages
+import 'dart:convert';
+import 'package:simple_spell_checker/simple_spell_checker.dart' show SimpleSpellChecker, languagesToBeUsed;
+import 'package:simple_spell_checker_es_lan/src/es/join_es_words.dart';
+
+class SimpleSpellCheckerEnRegister {
+  static const _splitter = LineSplitter();
+
+  /// `registerEnglishLanguage` can be used to register manually the english
+  /// language to be supported by the `SimpleSpellChecker`
+  static void registerSpanishLanguage() {
+    if (languagesToBeUsed.containsKey('es')) return;
+    SimpleSpellChecker.setLanguage('es', _createDictionary(joinSpanishWords));
+  }
+
+  static Map<String, int> _createDictionary(String words) {
+    if (words.trim().isEmpty) {
+      return {};
+    }
+    final Iterable<MapEntry<String, int>> entries = _splitter.convert(words).map(
+          (element) => MapEntry(
+            element.trim().toLowerCase(),
+            1,
+          ),
+        );
+    return {}..addEntries(entries);
+  }
+
+  static void unRegisterEnglishLanguage() {
+    languagesToBeUsed.remove('es');
+  }
+}
