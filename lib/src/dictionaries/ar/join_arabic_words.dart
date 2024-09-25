ef1530a615e9afde63292919c39b8a163dbc537a
CatHood0 <santiagowmar@gmail.com>
2024-09-25 01:19:30 -0400

    Feat: support for register arabic language

--- lib/src/dictionaries/ar/join_arabic_words.dart
+++
@@ -1,4 +0,0 @@
-import 'package:simple_spell_checker/src/dictionaries/ar/ar_words1.dart';
-import '../../utils.dart';
-
-final joinArabicWords = removeUnnecessaryCharacters(arWords1, '-');
---
+++ simple_spell_checker_ar_lan/lib/src/ar/join_arabic_words.dart
@@ -0,0 +1,4 @@
+import 'package:simple_spell_checker/src/utils.dart';
+import 'package:simple_spell_checker_ar_lan/src/ar/ar_words1.dart';
+
+final joinArabicWords = removeUnnecessaryCharacters(arWords1, '-');
