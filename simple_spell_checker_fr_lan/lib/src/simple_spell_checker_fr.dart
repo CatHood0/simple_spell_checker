23491a6f1fe1770ff3eec18212ef50e2f70a8338
CatHood0 <santiagowmar@gmail.com>
2024-09-27 20:31:32 -0400

    Feat: support for register french language

---
+++ simple_spell_checker_fr_lan/lib/src/simple_spell_checker_fr.dart
@@ -0,0 +1,33 @@
+
+// ignore: depend_on_referenced_packages
+import 'dart:convert';
+import 'package:simple_spell_checker/simple_spell_checker.dart' show SimpleSpellChecker, languagesToBeUsed;
+import 'package:simple_spell_checker_fr_lan/src/fr/join_french_words.dart';
+
+class SimpleSpellCheckerFrRegister {
+  static const _splitter = LineSplitter();
+
+  /// `registerFrenchLanguage` can be used to register manually the french 
+  /// language to be supported by the `SimpleSpellChecker`
+  static void registerFrenchLanguage() {
+    if (languagesToBeUsed.containsKey('fr')) return;
+    SimpleSpellChecker.setLanguage('fr', _createDictionary(joinFrenchWords));
+  }
+
+  static Map<String, int> _createDictionary(String words) {
+    if (words.trim().isEmpty) {
+      return {};
+    }
+    final Iterable<MapEntry<String, int>> entries = _splitter.convert(words).map(
+          (element) => MapEntry(
+            element.trim().toLowerCase(),
+            1,
+          ),
+        );
+    return {}..addEntries(entries);
+  }
+
+  static void unRegisterFrenchLanguage() {
+    languagesToBeUsed.remove('fr');
+  }
+}
