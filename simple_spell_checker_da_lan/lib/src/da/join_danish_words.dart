5a5288539a73b48f5bdeb22269b014d548bf35fc
CatHood0 <santiagowmar@gmail.com>
2024-09-27 20:53:35 -0400

    Feat: support for register danish language

---
+++ simple_spell_checker_da_lan/lib/src/da/join_danish_words.dart
@@ -0,0 +1,4 @@
+import 'package:simple_spell_checker/simple_spell_checker.dart' show removeUnnecessaryCharacters;
+import 'package:simple_spell_checker_da_lan/src/da/da_words1.dart';
+
+final joinDanishWords = removeUnnecessaryCharacters(daWords1);
