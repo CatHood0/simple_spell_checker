b60e0604c89ca6175beb7384a2967c488ac4af7e
CatHood0 <santiagowmar@gmail.com>
2024-09-27 21:58:19 -0400

    Chore: removed splitted dictionaries

--- lib/src/dictionaries/et/join_estonian_words.dart
+++
@@ -1,4 +0,0 @@
-import 'package:simple_spell_checker/src/dictionaries/et/et_words1.dart';
-import 'package:simple_spell_checker/src/dictionaries/utils/dic_utils.dart';
-
-final joinEstonianWords = removeUnnecessaryCharacters(etWords1, '-');
