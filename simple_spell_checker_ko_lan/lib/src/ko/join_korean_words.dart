d3fd6aa4595b4f42805489ffcb1f4ed23b15fd4f
CatHood0 <santiagowmar@gmail.com>
2024-09-27 22:05:23 -0400

    Feat: support for register korean language

---
+++ simple_spell_checker_ko_lan/lib/src/ko/join_korean_words.dart
@@ -0,0 +1,4 @@
+import 'package:simple_spell_checker/src/utils.dart';
+import 'package:simple_spell_checker_ko_lan/src/ko/ko_words1.dart';
+
+final joinKoreanWords = removeUnnecessaryCharacters(koWords1);
---
+++ simple_spell_checker_ko_lan/lib/src/simple_spell_checker_ko.dart
@@ -0,0 +1,31 @@
+import 'dart:convert';
+import 'package:simple_spell_checker/simple_spell_checker.dart' show SimpleSpellChecker, languagesToBeUsed;
+import 'package:simple_spell_checker_ko_lan/src/ko/join_korean_words.dart';
+
+class SimpleSpellCheckerKoRegister {
+  static const _splitter = LineSplitter();
+
+  /// `registerKoreanLanguage` can be used to register manually the korean 
+  /// language to be supported by the `SimpleSpellChecker`
+  static void registerKoreanLanguage() {
+    if (languagesToBeUsed.containsKey('ko')) return;
+    SimpleSpellChecker.setLanguage('ko', _createDictionary(joinKoreanWords));
+  }
+
+  static Map<String, int> _createDictionary(String words) {
+    if (words.trim().isEmpty) {
+      return {};
+    }
+    final Iterable<MapEntry<String, int>> entries = _splitter.convert(words).map(
+          (element) => MapEntry(
+            element.trim().toLowerCase(),
+            1,
+          ),
+        );
+    return {}..addEntries(entries);
+  }
+
+  static void unRegisterKoreanLanguage() {
+    languagesToBeUsed.remove('ko');
+  }
+}
