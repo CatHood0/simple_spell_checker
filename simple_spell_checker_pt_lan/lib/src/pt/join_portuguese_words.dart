70eafdcfaba9590fa31982f642c8bf34532566aa
CatHood0 <santiagowmar@gmail.com>
2024-09-27 22:24:47 -0400

    Feat: support for register portuguese language

--- simple_spell_checker_pt_lan/lib/src/pt/join_portuguese_words.dart
+++ simple_spell_checker_pt_lan/lib/src/pt/join_portuguese_words.dart
@@ -1,6 +1,7 @@
-import 'package:simple_spell_checker/src/dictionaries/pt/pt_words1.dart';
-import 'package:simple_spell_checker/src/dictionaries/pt/pt_words2.dart';
-import 'package:simple_spell_checker/src/dictionaries/utils/dic_utils.dart';
+import 'package:simple_spell_checker/src/utils.dart';
+
+import 'pt_words1.dart';
+import 'pt_words2.dart';
 
 /// we use join functions instead getting dictionaries directly
 /// since the dictionaries are too bigger to be used in just one file
---
+++ simple_spell_checker_pt_lan/lib/src/simple_spell_checker_pt.dart
@@ -0,0 +1,31 @@
+import 'dart:convert';
+import 'package:simple_spell_checker/simple_spell_checker.dart' show SimpleSpellChecker, languagesToBeUsed;
+import 'package:simple_spell_checker_pt_lan/src/pt/join_portuguese_words.dart';
+
+class SimpleSpellCheckerPtRegister {
+  static const _splitter = LineSplitter();
+
+  /// `registerPortugueseLanguage` can be used to register manually the portuguese 
+  /// language to be supported by the `SimpleSpellChecker`
+  static void registerPortugueseLanguage() {
+    if (languagesToBeUsed.containsKey('pt')) return;
+    SimpleSpellChecker.setLanguage('pt', _createDictionary(joinPortugueseWords));
+  }
+
+  static Map<String, int> _createDictionary(String words) {
+    if (words.trim().isEmpty) {
+      return {};
+    }
+    final Iterable<MapEntry<String, int>> entries = _splitter.convert(words).map(
+          (element) => MapEntry(
+            element.trim().toLowerCase(),
+            1,
+          ),
+        );
+    return {}..addEntries(entries);
+  }
+
+  static void unRegisterPortugueseLanguage() {
+    languagesToBeUsed.remove('pt');
+  }
+}
